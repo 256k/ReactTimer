6ae26d8670720c7d977a9f277f72c407830cfb73
256k <fayek.helmi@gmail.com>
2016-10-23 21:30:46 -0400

    added all the necessary files. set up the testing infrastructure using karma and mocha.

---
+++ app/components/nav.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+
+var Nav = React.createClass({
+    render: function() {
+        return (
+            <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                <li className="menu-text">
+                    Timer.
+                </li>
+                <li>
+                    <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+                </li>
+                <li>
+                    <Link to="/" activeClassName="active-link">Countdown</Link>
+                </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                <li className="menu-text">
+                    <p>Created By <a href="http://www.256klabs.com" target="_blank">Fayek Helmi</a></p>
+                </li>
+                </ul>
+            </div>
+            </div>
+
+        );
+    }
+});
+
+module.exports = Nav;
---
+++ readme.md
@@ -0,0 +1,3 @@
+Timer app
+===
+Timer app for Udemy React course + testing cases and writing testing code and getting app to production level.
